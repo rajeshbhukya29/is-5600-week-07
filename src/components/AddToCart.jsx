e1809389d1cab764dc128c3a5a3a43753a29607a
rajeshbhukya29 <rajesh.bhukya@slu.edu>
2025-05-06 19:39:34 +0000

    Changed added to cart

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -13,5 +13,4 @@ export default function OrderButton({product}) {
     return (
       <a className="f6 link dim br3 ba bw1 ph3 pv2 mb2 dib black" onClick={() => handleClick(product)}>Add to Cart</a>
     )
-  }
-  
+}
